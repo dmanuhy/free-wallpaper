56ddd716c9dd85ec5fa395af7a7ec0c40800ef6f
Manh <manuhy04@gmail.com>
2024-06-13 18:22:57 +0700

    [DEV][FE] Search Result/Category

--- free-wallpaper-front-end/src/components/Search/Search.js
+++ free-wallpaper-front-end/src/components/Search/Search.js
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom"
 import "./Search.scss"
+import { useState } from "react";
 
 const Search = () => {
+
+    const naviage = useNavigate();
+    const [searchValue, setSearchValue] = useState("");
+
+
+    const handleSeachOnEnter = (key) => {
+        if (key === "Enter") {
+            naviage(`search/${searchValue}`)
+        }
+    }
+
     return (
         <div className="search">
-            <input className="search-input col-10" type="text" placeholder='Seach for free Wallpaper ...' />
-            <i className="search-icon fa-solid fa-magnifying-glass"></i>
+            <input onKeyDown={(event) => handleSeachOnEnter(event.key)} onChange={(event) => setSearchValue(event.target.value)} value={searchValue} className="search-input col-10" type="text" placeholder='Seach for free Wallpaper ...' />
+            <i onClick={() => naviage(`search/${searchValue}`)} className="search-icon fa-solid fa-magnifying-glass"></i>
         </div>
     )
 }
