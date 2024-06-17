a51a355ace702c5aa8792914099f7e73abc45338
vanquang2002 <132914620+vanquang2002@users.noreply.github.com>
2024-06-17 21:42:49 +0700

    Update wallpaper.js

---
+++ free-wallpaper-back-end/models/wallpaper.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+const WallpaperSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
