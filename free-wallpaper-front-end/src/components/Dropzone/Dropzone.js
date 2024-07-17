676e49495a482f2d9cfa652c9e49eb52282fcbf2
Hminh11 <minhvhhe170320@fpt.edu.vn>
2024-07-17 21:37:48 +0700

    fix

--- free-wallpaper-front-end/src/components/Dropzone/Dropzone.js
+++ free-wallpaper-front-end/src/components/Dropzone/Dropzone.js
@@ -1,16 +1,26 @@
 import { useDropzone } from "react-dropzone";
 import iconUploadImage from "../../assets/icon/icon-upload-image.png";
 import "./Dropzone.scss";
+import { useState } from 'react';
 
 const Dropzone = ({ newImages, setNewImages }) => {
+    const [fileSizeError, setFileSizeError] = useState("");
+
     const { getRootProps, getInputProps, isDragActive } = useDropzone({
         accept: "image/*",
         maxFiles: 10,
-        onDrop: (acceptedFiles) => {
-            acceptedFiles.map((uploadedFile) => {
+        maxSize: 5242880,
+        onDrop: (acceptedFiles, rejectedFiles) => {
+            const validFiles = acceptedFiles.map((uploadedFile) => {
                 return Object.assign(uploadedFile, { imageUrl: URL.createObjectURL(uploadedFile) });
             });
-            setNewImages([...newImages, ...acceptedFiles]);
+            setNewImages([...newImages, ...validFiles]);
+
+            if (rejectedFiles.length > 0) {
+                setFileSizeError("Some files were too large and were not added. Maximum file size is 5MB.");
+            } else {
+                setFileSizeError("");
+            }
         },
     });
 
@@ -22,33 +32,28 @@ const Dropzone = ({ newImages, setNewImages }) => {
 
     return (
         <>
-            {!newImages || newImages.length < 1 ? (
-                <div {...getRootProps()} className="dropzone">
+            <div {...getRootProps()} className="dropzone">
+                <input {...getInputProps()} />
+                {!isDragActive ? (
                     <>
                         <img className="dropzone-icon" src={iconUploadImage} alt="upload" />
-                        {!isDragActive ? (
-                            <h3 className="text-nowrap">Drag and drop to upload</h3>
-                        ) : (
-                            <h3>Drop Image Here</h3>
-                        )}
+                        <h3 className="text-nowrap">Drag and drop to upload</h3>
                         <h5>OR</h5>
                         <button className="btn btn-outline-success fs-4">Open Browse</button>
                     </>
-                </div>
-            ) : (
-                <div className="d-flex flex-column gap-4">
-                    {newImages.map((item, index) => {
-                        return (
-                            <div className="dropzone-item d-flex justify-content-between" key={index}>
-                                <div {...getRootProps()} className="dropzone">
-                                    <img className="dropzone-image" src={item.imageUrl} alt="wallpaper" />
-                                </div>
-                                <i onClick={() => handleDeleteImage(index)} className="dropzone-item-delete fa-solid fa-trash-can"></i>
-                            </div>
-                        );
-                    })}
-                </div>
-            )}
+                ) : (
+                    <h3>Drop Image Here</h3>
+                )}
+            </div>
+            {fileSizeError && <div className="text-danger error-message">{fileSizeError}</div>}
+            <div className="d-flex flex-column gap-4">
+                {newImages.map((item, index) => (
+                    <div className="dropzone-item d-flex justify-content-between" key={index}>
+                        <img className="dropzone-image" src={item.imageUrl} alt="wallpaper" />
+                        <i onClick={() => handleDeleteImage(index)} className="dropzone-item-delete fa-solid fa-trash-can"></i>
+                    </div>
+                ))}
+            </div>
         </>
     );
 };
